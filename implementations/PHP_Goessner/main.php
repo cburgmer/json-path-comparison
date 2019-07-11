52b270cd8439d5c2c454fbdfc35ac85e71328a39
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-07-11 22:32:14 +0200

    Add Goessner's original PHP implementation for reference

---
+++ implementations/PHP_Goessner/main.php
@@ -0,0 +1,17 @@
+<?php
+// disable absolute paths showing up in output
+error_reporting(E_ERROR | E_PARSE);
+
+require_once('jsonpath.php');
+
+$json = file_get_contents("php://stdin");
+$o = json_decode($json, true);
+$result = jsonPath($o, $argv[1]);
+
+if ($result === false) {
+    print 'jsonpath returned false, this might indicate an error';
+    die(1);
+} else {
+    echo json_encode($result);
+}
+?>
