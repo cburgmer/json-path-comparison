843d1284b8efbb73758a20a162f68264445cab58
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-07-11 21:18:57 +0200

    Corner case step 0
    Goessner's original implementation runs into an out of memory issue, needs a work around to not spill an ever changing stack trace into the logs

--- implementations/JavaScript_Goessner/run.sh
+++ implementations/JavaScript_Goessner/run.sh
@@ -18,4 +18,20 @@ if [[ ! -f "$jsonpath_export" ]]; then
     } > "$jsonpath_export"
 fi
 
-./index.js "$@"
+readonly tmp_stdout="/tmp/node_goessner_jsonpath.stdout.$$"
+readonly tmp_stderr="/tmp/node_goessner_jsonpath.stderr.$$"
+
+output_and_cleanup() {
+  local filter_out_of_memory="CALL_AND_RETRY_LAST Allocation failed"
+  if ! grep "$filter_out_of_memory" < "$tmp_stderr"; then
+    cat "$tmp_stdout"
+    >&2 cat "$tmp_stderr"
+  fi
+  rm "$tmp_stdout"
+  rm "$tmp_stderr"
+}
+
+trap output_and_cleanup EXIT
+
+# https://stackoverflow.com/questions/34964332/suppress-signal-output-like-abort-trap-6-in-a-shellscript-for-a-test-that-sh
+$(node --max-old-space-size=10 index.js "$@" > "$tmp_stdout" 2> "$tmp_stderr")
