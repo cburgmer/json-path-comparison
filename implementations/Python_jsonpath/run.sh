0a16d66a9ec2ed8571df05fc43ad0b4b99442921
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-07-20 17:27:06 +0200

    Fix platform differences in Python site-packages path

--- implementations/Python_jsonpath/run.sh
+++ implementations/Python_jsonpath/run.sh
@@ -5,4 +5,4 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$script_dir"
 
-PYTHONPATH="$script_dir"/deps/usr/local/lib/python3.7/site-packages/ python3 main.py "$@"
+PYTHONPATH="$( cd deps/lib/python*/site-packages/ && pwd )" python3 main.py "$@"
