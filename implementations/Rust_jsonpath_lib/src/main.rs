164a1143c1d9b7479b175f26230bfb989ad041aa
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-07-21 15:00:43 +0200

    Don't leak Rust stacktraces into logs as they are different across platforms

--- implementations/Rust_jsonpath_lib/src/main.rs
+++ implementations/Rust_jsonpath_lib/src/main.rs
@@ -3,13 +3,31 @@ extern crate serde_json;
 
 use std::io;
 use std::env;
+use std::process;
+use std::panic;
 
 fn main() {
+    panic::set_hook(Box::new(|_| {
+        println!("panic occurred");
+    }));
+
     let query = env::args().nth(1).unwrap();
 
-    let json = serde_json::from_reader(io::stdin()).unwrap();
+    let json = match serde_json::from_reader(io::stdin()) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1);
+        }
+    };
     let mut selector = jsonpath::selector(&json);
 
-    let result = selector(&query[..]).unwrap();
+    let result = match selector(&query[..]) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1);
+        }
+    };
     serde_json::to_writer(io::stdout(), &result).unwrap();
 }
