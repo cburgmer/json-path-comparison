c583d4402e6a78bc3271574b67659052c3811aff
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-09-14 21:33:52 +0200

    Decrease timeout to avoid out of memory to trigger before yielding a different error message

--- implementations/C_json-glib/run.sh
+++ implementations/C_json-glib/run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Timeout after some seconds as slices with step seem to go into an endless loop
-timeout 10 "$script_dir"/build/main "$@"
+timeout 5 "$script_dir"/build/main "$@"
