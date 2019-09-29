190859658fc05352189d2c9d8a580ef5d301a7f6
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-09-29 23:35:10 +0200

    Refactor, simplify

--- implementations/Bash_JSONPath.sh/run.sh
+++ implementations/Bash_JSONPath.sh/run.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-readonly tmp_output="/tmp/Bash_JSONPath.sh.$$"
 
 cd "$script_dir"
-./ensure_deps.sh ./build/JSONPath.sh -u -j "$@" | python3 -m json.tool | tee "$tmp_output"
-
-# Catch empty output
-if [[ -z "$(tr -d '\n' < "$tmp_output")" ]]; then
-    rm "$tmp_output"
-    exit 1
-fi
-
-rm "$tmp_output"
+# Piping everything through a JSON parser to ensure we fail hard on a very lenient implementation
+./ensure_deps.sh ./build/JSONPath.sh -u -j "$@" | python3 -m json.tool
