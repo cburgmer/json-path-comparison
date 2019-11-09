c68d4241a58fee6975dad421a65006e6dfc27769
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-11-09 18:02:25 +0100

    Remove hotfix as PR has been merged

--- implementations/Bash_JSONPath.sh/install.sh
+++ implementations/Bash_JSONPath.sh/install.sh
@@ -2,15 +2,8 @@
 set -euo pipefail
 
 readonly source_url="https://raw.githubusercontent.com/mclarkson/JSONPath.sh/master/JSONPath.sh"
-readonly tmp_target="/tmp/JSONPath.sh.$$"
-
 readonly target="$1"
 
-curl --fail -Lo "$tmp_target" "$source_url"
-
-# Work around https://github.com/mclarkson/JSONPath.sh/pull/7
-sed 's/%0${indent}s/%${indent}s/' < "$tmp_target" > "$target"
+curl --fail -Lo "$target" "$source_url"
 
 chmod a+x "$target"
-
-rm "$tmp_target"
