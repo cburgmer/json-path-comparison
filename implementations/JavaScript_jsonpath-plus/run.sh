adcbe82e65f5864c2829a5e87d215762048e17b8
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-12-04 23:43:49 +0100

    Query for filter on current value

--- implementations/JavaScript_jsonpath-plus/run.sh
+++ implementations/JavaScript_jsonpath-plus/run.sh
@@ -9,4 +9,19 @@ if [[ ! -d "node_modules" ]]; then
     npm install
 fi
 
-./index.js "$@"
+readonly tmp_stdout="node_jsonpath-plus.stdout.$$"
+readonly tmp_stderr="node_jsonpath-plus.stderr.$$"
+
+output_and_cleanup() {
+    local filter_syntax_error_in_vm="evalmachine.<anonymous>:1"
+    if ! grep "$filter_syntax_error_in_vm" < "$tmp_stdout"; then
+        cat "$tmp_stdout"
+        >&2 cat "$tmp_stderr"
+    fi
+    rm "$tmp_stdout"
+    rm "$tmp_stderr"
+}
+
+trap output_and_cleanup EXIT
+
+./index.js "$@" > "$tmp_stdout" 2> "$tmp_stderr"
