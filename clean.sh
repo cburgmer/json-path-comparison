51c6758b97c98616e2fbfd229d7d8b8d3430d2f1
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-12-06 18:52:03 +0100

    Clean more rigorously

--- clean.sh
+++ clean.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker rmi json-path-comparison
+docker rmi json-path-comparison || true
+rm -r cabal-cache || true
 find "$script_dir" -type d -name "build" | xargs rm -r
+find "$script_dir" -type d -name "deps" | xargs rm -r
