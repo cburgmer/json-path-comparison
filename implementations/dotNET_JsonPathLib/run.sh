9ee96a44c316cd9f18494503cb0b54b0f1be444f
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-12-30 18:05:14 +0100

    Handle timeout in Dotnet_JsonPathLib

--- implementations/dotNET_JsonPathLib/run.sh
+++ implementations/dotNET_JsonPathLib/run.sh
@@ -2,4 +2,7 @@
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$script_dir"/build/Dotnet_JsonPathLib "$@"
+cd "$script_dir"
+
+# Timeout after some seconds as slices with 0 step seem to go into an infinite loop
+timeout -v 10 build/Dotnet_JsonPathLib "$@"
