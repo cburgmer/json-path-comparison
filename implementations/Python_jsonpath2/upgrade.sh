8253ec23f478d383ed8ab9851723472baa61a65a
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-01-25 12:40:39 +0100

    Fix upgrade.sh and bump dependencies for Python implementations

--- implementations/Python_jsonpath2/upgrade.sh
+++ implementations/Python_jsonpath2/upgrade.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd "$(dirname "$BASH_SOURCE[0]")"
-PYTHONPATH="$( cd deps/lib/python*/site-packages/ && pwd )" ./deps/bin/pur -r requirements.txt
+PYTHONPATH="$( cd build/deps/lib/python*/site-packages/ && pwd )" ./build/deps/bin/pur -r requirements.txt
