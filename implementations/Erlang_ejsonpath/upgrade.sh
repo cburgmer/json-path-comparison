efed151d129ee6c0b66fac5ce9ceebda64d446d4
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-02-03 20:12:06 +0100

    Stop rebuilding Erlang everytime. Work around https://github.com/erlang/rebar3/issues/2220

--- implementations/Erlang_ejsonpath/upgrade.sh
+++ implementations/Erlang_ejsonpath/upgrade.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "$(dirname "$BASH_SOURCE[0]")"
+readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${script_dir}"
+
 rebar3 upgrade
+# Poor man's dependency handling, work around https://github.com/erlang/rebar3/issues/2220
+rm -r ./build/
