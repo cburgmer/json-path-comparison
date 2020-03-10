549ea7a4d939a9fc870cdc7470f5a1f6f17defc8
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-03-10 21:10:00 +0100

    Correcly pass quotes from selectors when running in docker

--- src/wrap_in_docker.sh
+++ src/wrap_in_docker.sh
@@ -2,9 +2,8 @@
 set -euo pipefail
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-readonly RUN_ARGS="$@"
 readonly target_image=json-path-comparison
 
 docker build -t "$target_image" "$script_dir"
 
-docker run --rm -v "$(pwd):/json-path-comparison" -i "$target_image" sh -c "cd /json-path-comparison && $RUN_ARGS"
+docker run --rm -v "$(pwd):/json-path-comparison" -w "/json-path-comparison" -i "$target_image" "$@"
