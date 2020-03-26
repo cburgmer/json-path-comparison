35d138ff8fb96834c7910d562a9d5960c84b3a1a
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-03-26 22:53:48 +0100

    Array slice

--- proposal/operators.js
+++ proposal/operators.js
@@ -1,12 +1,19 @@
 const isArray = (v) => Array.isArray(v);
 const isObject = (v) => !isArray(v) && typeof v === "object" && v !== null;
 
-const childrenIndexOperator = (value, child) => {
+const realIndex = (sliceIndex, length) => {
+  if (sliceIndex < 0) {
+    return Math.max(0, length + sliceIndex);
+  }
+  return sliceIndex;
+};
+
+const childrenIndexOperator = (value, [child]) => {
   if (isArray(value)) {
-    const index = child - 0; // HACKY way to convert complete string to int
-    const realIndex = index >= 0 ? index : value.length + index;
-    if (realIndex >= 0 && realIndex < value.length) {
-      return [value[realIndex]];
+    const index = parseInt(child, 10);
+    const realIdx = realIndex(index, value.length);
+    if (realIdx >= 0 && realIdx < value.length) {
+      return [value[realIdx]];
     }
   } else if (isObject(value)) {
     if (value[child] !== undefined) {
@@ -26,12 +33,44 @@ const childrenAllOperator = (value) => {
   return [];
 };
 
+const sliceValueOrDefault = (sliceValue, defaultValue) => {
+  if (sliceValue === null) {
+    return defaultValue;
+  }
+  return parseInt(sliceValue, 10);
+};
+
+const childrenSliceOperator = (value, [start, end, step]) => {
+  if (isArray(value)) {
+    const slice = [];
+
+    const stepNumber = sliceValueOrDefault(step, 1);
+
+    const realStart = realIndex(sliceValueOrDefault(start, 0), value.length);
+    const realEnd = realIndex(
+      sliceValueOrDefault(end, value.length),
+      value.length
+    );
+
+    for (let i = realStart; i < realEnd; i += stepNumber) {
+      if (0 <= i && i < value.length) {
+        slice.push(value[i]);
+      }
+    }
+    return slice;
+  }
+
+  return [];
+};
+
 module.exports.childrenOperator = (value, children) => {
-  return children.flatMap(([subOperator, child]) => {
+  return children.flatMap(([subOperator, ...parameters]) => {
     if (subOperator === "index") {
-      return childrenIndexOperator(value, child);
+      return childrenIndexOperator(value, parameters);
     } else if (subOperator === "all") {
       return childrenAllOperator(value);
+    } else if (subOperator === "slice") {
+      return childrenSliceOperator(value, parameters);
     }
     throw new Error("Internal error, unknown operator");
   });
