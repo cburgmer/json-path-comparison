727eadb2315166c1bab773b4d9024008427000e1
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-03-28 15:57:25 +0100

    Remove locale override on timeout. Not needed anymore

--- implementations/C_json-glib/run.sh
+++ implementations/C_json-glib/run.sh
@@ -5,4 +5,4 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$script_dir"
 # Timeout after some seconds as slices with step seem to go into an endless loop
-LC_CTYPE=C timeout -v 5 build/main "$@"
+timeout -v 5 build/main "$@"
--- implementations/dotNET_JsonPathLib/run.sh
+++ implementations/dotNET_JsonPathLib/run.sh
@@ -5,4 +5,4 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$script_dir"
 
 # Timeout after some seconds as slices with 0 step seem to go into an infinite loop
-LC_CTYPE=C timeout -v 10 build/Dotnet_JsonPathLib "$@"
+timeout -v 10 build/Dotnet_JsonPathLib "$@"
