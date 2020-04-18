555a770f7cf09386fa9d62f8ce00202059fac731
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-04-18 18:05:10 +0200

    Try fail with better parse errors

--- proposal/index.js
+++ proposal/index.js
@@ -1,8 +1,22 @@
+const Tracer = require("pegjs-backtrace");
 const { parse } = require("./selector");
 const execute = require("./operators");
 
+class SyntaxError extends Error {
+  constructor(backtrace) {
+    super("\n" + backtrace);
+    this.name = "SyntaxError";
+  }
+}
 const jsonpath = (selector, json) => {
-  const operators = parse(selector);
+  let operators;
+  const tracer = new Tracer(selector);
+  try {
+    operators = parse(selector, { tracer });
+  } catch (e) {
+    throw new SyntaxError(tracer.getBacktraceString());
+  }
+
   return execute(json, operators);
 };
 
