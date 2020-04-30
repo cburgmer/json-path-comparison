49dff00dce6b79522a2f7e2b52024d5b107f6931
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-04-30 22:06:49 +0200

    Fix parsing and serialization of simple JSON values in ObjectiveC implementation

--- implementations/Objective-C_SMJJSONPath/main.m
+++ implementations/Objective-C_SMJJSONPath/main.m
@@ -3,6 +3,42 @@
 
 #define NSLog(FORMAT, ...) fprintf(stderr, "%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
 
+@interface PoormansRFC7159ConformingJSONSerialization : NSObject
+
++ (id)JSONObjectWithData:(NSData *)data
+                   error:(NSError * _Nullable *)error;
+
++ (NSData *)dataWithJSONObject:(id)obj
+                         error:(NSError * _Nullable *)error;
+
+@end
+
+
+@implementation PoormansRFC7159ConformingJSONSerialization
+
++ (id)JSONObjectWithData:(NSData *)data
+                   error:(NSError * _Nullable *)error {
+    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
+    NSString *augmentedJson = [NSString stringWithFormat:@"[%@]", jsonString];
+    NSData *augmentedData = [augmentedJson dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSArray *jsonArray = [NSJSONSerialization JSONObjectWithData:augmentedData options:0 error:error];
+    return jsonArray[0];
+}
+
++ (NSData *)dataWithJSONObject:(id)obj
+                         error:(NSError * _Nullable *)error {
+    NSArray *array = [NSArray arrayWithObjects: obj, nil];
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:array options:0 error:error];
+
+    NSRange range = {1, [jsonData length] - 2};
+    return [jsonData subdataWithRange:range];
+}
+
+@end
+
+
 int main (int argc, const char * argv[]) {
     NSString *selector = [NSString stringWithUTF8String:argv[1]];
 
@@ -11,7 +47,7 @@ int main (int argc, const char * argv[]) {
     NSFileHandle *kbd = [NSFileHandle fileHandleWithStandardInput];
     NSData *data = [kbd availableData];
 
-    id jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    id jsonObject = [PoormansRFC7159ConformingJSONSerialization JSONObjectWithData:data error:&error];
     if (error) {
         NSLog(@"%@", error.localizedDescription);
         return 1;
@@ -36,17 +72,12 @@ int main (int argc, const char * argv[]) {
         return 1;
     }
 
-    // poorman's JSON serialization
-    if ([result isKindOfClass:[NSString class]]) {
-        printf("\"%s\"\n", [result UTF8String]);
-    } else if ([result isKindOfClass:[NSNumber class]]) {
-        printf("%d\n", [result intValue]);
-    } else {
-        // NSLog(@"Is of type: %@", [result class]);
-
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:0 error:&error];
-        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-
-        printf("%s\n", [jsonString UTF8String]);
+    NSData *jsonData = [PoormansRFC7159ConformingJSONSerialization dataWithJSONObject:result error:&error];
+    if (error) {
+        NSLog(@"%@", error.localizedDescription);
+        return 1;
     }
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+
+    printf("%s\n", [jsonString UTF8String]);
 }
