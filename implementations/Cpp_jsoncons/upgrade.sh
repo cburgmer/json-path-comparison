eb8945cd824edc13dba1582e50233c9dc04b47b4
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-05-03 16:26:02 +0200

    Bump Objective-C_SMJJSONPath and provide upgrade mechanism

--- implementations/Cpp_jsoncons/upgrade.sh
+++ implementations/Cpp_jsoncons/upgrade.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 cd "$(dirname "$BASH_SOURCE[0]")"
 curl 'https://api.github.com/repos/danielaparker/jsoncons/releases' | grep '"tag_name":' | head -1 | sed 's/.*"tag_name": "\(.*\)",.*/\1/' > tag
-rm -r build
+rm -rf build
---
+++ implementations/Objective-C_SMJJSONPath/upgrade.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$BASH_SOURCE[0]")"
+curl 'https://api.github.com/repos/javerous/SMJJSONPath/releases' | grep '"tag_name":' | head -1 | sed 's/.*"tag_name": "\(.*\)",.*/\1/' > tag
+rm -rf build
