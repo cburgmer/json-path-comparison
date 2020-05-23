5d9f61dd8ecc9f7de80f3b64062f824e6b06f2fe
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-05-23 21:52:04 +0200

    Allow majority rules to handle excceptions. #37

--- src/build_majority.sh
+++ src/build_majority.sh
@@ -3,23 +3,19 @@ set -euo pipefail
 
 readonly query_results="$1"
 
-. src/shared.sh
-
 all_implementations() {
     find ./implementations -name run.sh -maxdepth 2 -print0 | xargs -0 -n1 dirname | xargs -n1 basename
 }
 
-all_ok_implementation_results() {
+all_implementation_results() {
     local implementation
     while IFS= read -r implementation; do
-        if is_query_result_ok "${query_results}/${implementation}"; then
-            echo "${query_results}/${implementation}"
-        fi
+        echo "${query_results}/${implementation}"
     done <<< "$(all_implementations)"
 }
 
 majority_members() {
-    all_ok_implementation_results | ./src/majority.py
+    all_implementation_results | ./src/majority.py
 }
 
 main() {
