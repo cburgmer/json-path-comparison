da46bda0f919dbcdd4350cd01e6f0890e938ebb0
Nikolay Feldman <niko@nfeld.com>
2020-06-18 20:23:12 -0700

    fix json output format

--- implementations/Kotlin_com.nfeld.jsonpathkt/src/main/kotlin/query/App.kt
+++ implementations/Kotlin_com.nfeld.jsonpathkt/src/main/kotlin/query/App.kt
@@ -1,15 +1,17 @@
 package query
 
 import kotlin.system.exitProcess
+import com.fasterxml.jackson.databind.JsonNode
 import com.nfeld.jsonpathkt.extension.read
 import com.nfeld.jsonpathkt.JsonPath
 import com.google.gson.Gson
+import com.google.gson.JsonParser
 
 fun main(args : Array<String>) {
     val json = generateSequence(::readLine).joinToString("\n")
     try {
-        val results = JsonPath.parse(json)?.read<Any>(args[0])
-        println(results?.toString())
+        val results = JsonPath.parse(json)?.read<JsonNode>(args[0])?.toString()
+        println(Gson().toJson(JsonParser().parse(results)))
     } catch (e: IllegalArgumentException) {
         System.err.println(e.message);
         exitProcess(2);
