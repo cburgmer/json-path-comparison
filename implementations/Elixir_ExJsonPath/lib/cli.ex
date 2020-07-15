fcac2afce61acc97075abdfd0cd5c24384ffa9b8
Cleidiano Oliveira <cleidiano.souza@guiabolso.com.br>
2020-07-15 13:15:23 -0300

    Improve exceptions handling

--- implementations/Elixir_ExJsonPath/lib/cli.ex
+++ implementations/Elixir_ExJsonPath/lib/cli.ex
@@ -1,7 +1,9 @@
 defmodule Jsonpath.CLI do
   def encode(j) do
     case Poison.encode(j) do
-      {:ok, result} -> result
+      {:ok, result} ->
+        result
+
       {:error, {:invalid, object}} ->
         IO.puts("Invalid object returned")
         System.halt(1)
@@ -11,29 +13,28 @@ defmodule Jsonpath.CLI do
   def run(j, selector) do
     ExJSONPath.eval(j, selector)
   rescue
-    exception in [FunctionClauseError, WithClauseError, Protocol.UndefinedError] ->
+    exception ->
       {:error, exception}
   end
 
   def main(args) do
     json = IO.read(:stdio, :all)
     selector = hd(args)
+
     case run(Poison.decode!(json), selector) do
-      {:ok, result} -> IO.puts(encode(result))
+      {:ok, result} ->
+        IO.puts(encode(result))
+
       {:error, %ExJSONPath.ParsingError{message: message}} ->
         IO.puts(message)
         System.halt(2)
-      {:error, %FunctionClauseError{}} ->
-        IO.puts("FunctionClauseError")
+
+      {:error, %{__exception__: true, __struct__: module_name}} ->
+        IO.puts(inspect(module_name))
         System.halt(1)
-      {:error, %WithClauseError{}} ->
-        IO.puts("WithClauseError")
-        System.halt(1)
-      {:error, %Protocol.UndefinedError{}} ->
-        IO.puts("Protocol.UndefinedError")
-        System.halt(1)
+
       {:error, error} ->
-        IO.puts(error)
+        IO.puts("#{inspect(error)}")
         System.halt(1)
     end
   end
--- implementations/Elixir_warpath/lib/cli.ex
+++ implementations/Elixir_warpath/lib/cli.ex
@@ -2,29 +2,27 @@ defmodule Jsonpath.CLI do
   def run(j, selector) do
     Warpath.query(j, selector)
   rescue
-    exception in [FunctionClauseError, Jason.DecodeError] ->
-      {:error, exception}
+    exception -> {:error, exception}
   end
 
   def main(args) do
     json = IO.read(:stdio, :all)
     selector = hd(args)
+
     case run(Poison.decode!(json), selector) do
-      {:ok, result} -> IO.puts(Poison.encode!(result))
+      {:ok, result} ->
+        IO.puts(Poison.encode!(result))
+
       {:error, %Warpath.ExpressionError{message: message}} ->
         IO.puts(message)
         System.halt(2)
-      {:error, {:unsupported_operation, message}} ->
-        IO.puts(message)
+
+      {:error, %{__exception__: true, __struct__: module_name}} ->
+        IO.puts(inspect(module_name))
         System.halt(1)
-      {:error, %Jason.DecodeError{}} ->
-        IO.puts("Jason.DecodeError")
-        System.halt(1)
-      {:error, %FunctionClauseError{}} ->
-        IO.puts("FunctionClauseError")
-        System.halt(1)
-      {:error, exception} ->
-        IO.puts(exception)
+
+      {:error, error} ->
+        IO.puts("#{inspect(error)}")
         System.halt(1)
     end
   end
