f104a40b717bd2b07d2092084fb727780aa94710
Greg Dennis <gregsdennis@yahoo.com>
2020-10-18 13:25:13 +1300

    added JsonPath.Net from gregsdennis/json-everything

---
+++ implementations/dotNET_JsonPath.Net/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using Json.More;
+using Json.Path;
+
+
+namespace Dotnet_Manatee.Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = "";
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                jsonString += s;
+            }
+            
+            var o = JsonDocument.Parse(jsonString).RootElement;
+            var selector = args[0];
+            JsonPath path = null;
+            try
+            {
+                path = JsonPath.Parse(selector);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(2);
+            }
+            try
+            {
+                var results = path.Evaluate(o);
+                Console.WriteLine(results.Matches.Select(m => m.Value).AsJsonElement().ToJsonString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ implementations/dotNET_JsonPath.Net/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+"$script_dir"/build/Dotnet_JsonPath.Net "$@"
