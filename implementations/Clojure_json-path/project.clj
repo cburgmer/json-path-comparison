51829cbf3c4dfc33ca240a9cc9708e657ebb502f
Christoph Burgmer <christoph.burgmer@gmail.com>
2020-12-28 17:02:23 +0100

    Allow upgrade of Clojure_json-path

--- implementations/Clojure_json-path/project.clj
+++ implementations/Clojure_json-path/project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cheshire "5.8.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.10.0"]
                  [json-path "2.1.0"]]
   :main ^:skip-aot json-path-comparison.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-ancient "0.6.15"]]}})
