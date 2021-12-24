a46cdabb1322c66e412ed00de49f231b2a64086a
Rocco Bowling <rocco@chimerasw.com>
2021-12-24 00:44:18 -0500

    fixes for Swift_Sextant

--- implementations/Swift_Sextant/Sources/Swift_Sextant/main.swift
+++ implementations/Swift_Sextant/Sources/Swift_Sextant/main.swift
@@ -12,20 +12,9 @@ while let line = readLine() {
     input = input + line + "\n"
 }
 
-guard let inputData = input.data(using: String.Encoding.utf8 ) else {
-    print("Can't decode stdin")
-    exit(1)
-}
-
-let inputJson: JsonAny = try? JSONSerialization.jsonObject(with: inputData, options: .allowFragments)
-if inputJson == nil {
-    print("Can't deserialize input")
-    exit(1)
-}
-
-guard let result = inputJson.query(values: selector) else {
-    print("Can't execute query")
-    exit(1)
+guard let result = input.query(values: selector) else {
+    print("NOT_SUPPORTED")
+    exit(2)
 }
 
 guard let resultJson = try? JSONSerialization.data(withJSONObject: result, options: [.fragmentsAllowed]) else {
@@ -33,4 +22,6 @@ guard let resultJson = try? JSONSerialization.data(withJSONObject: result, optio
     exit(1)
 }
 
-print(resultJson)
+if let resultJsonString = String(data: resultJson, encoding: .utf8) {
+    print(resultJsonString)
+}
