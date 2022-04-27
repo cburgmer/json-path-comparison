c0a5122a7c6ae8923550e7208d6443be79bc94d0
Christoph Burgmer <christoph.burgmer@gmail.com>
2022-04-27 22:58:23 +0200

    Bump Clojure_json-path

--- implementations/Clojure_json-path/project.clj
+++ implementations/Clojure_json-path/project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [cheshire "5.10.2"]
-                 [json-path "2.1.0"]]
+                 [json-path "2.2.0"]]
   :main ^:skip-aot json-path-comparison.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
