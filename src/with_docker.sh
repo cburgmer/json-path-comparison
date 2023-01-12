d92cc9f16c88a9d25af34911bd2719a4d265fc01
Yusuke Asai <prometric2000@gmail.com>
2023-01-13 00:05:58 +0900

    Give environment file to docker command

--- src/with_docker.sh
+++ src/with_docker.sh
@@ -6,4 +6,4 @@ readonly target_image=json-path-comparison
 
 docker build -t "$target_image" "$script_dir"
 
-docker run --rm -v "$(pwd):/json-path-comparison" -w "/json-path-comparison" -i "$target_image" "$@"
+docker run --env-file="${script_dir}/docker_env_file.txt" --rm -v "$(pwd):/json-path-comparison" -w "/json-path-comparison" -i "$target_image" "$@"
