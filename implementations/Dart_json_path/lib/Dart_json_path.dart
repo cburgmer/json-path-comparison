8e10511e00dc2d99334c68c9b3d98acea2e58f8c
f3ath <karapetov@gmail.com>
2023-04-11 20:39:10 -0700

    Bump Dart implementation version to 0.5

--- implementations/Dart_json_path/lib/Dart_json_path.dart
+++ implementations/Dart_json_path/lib/Dart_json_path.dart
@@ -5,8 +5,5 @@ import 'package:json_path/json_path.dart';
 String select(String selector, String payload) {
   final json = jsonDecode(payload);
 
-  return jsonEncode(JsonPath(selector)
-    .read(json)
-    .map((result) => result.value).toList()
-  );
+  return jsonEncode(JsonPath(selector).readValues(json).toList());
 }
