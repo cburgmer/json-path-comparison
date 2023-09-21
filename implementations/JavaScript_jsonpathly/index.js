851c0013538a3450798c54e61d3d5af683484ecf
atamano <antoine.tamano@gmail.com>
2023-09-21 18:36:29 +0200

    add javscript jsonpathly implementation

---
+++ implementations/JavaScript_jsonpathly/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const jp = require("jsonpathly");
+
+const selector = process.argv[2];
+
+const stdin = process.stdin,
+  stdout = process.stdout,
+  inputChunks = [];
+
+stdin.resume();
+stdin.setEncoding("utf8");
+
+stdin.on("data", function (chunk) {
+  inputChunks.push(chunk);
+});
+
+stdin.on("end", function () {
+  const input = inputChunks.join(),
+    json = JSON.parse(input);
+
+  try {
+    const result = jp.query(json, selector, { returnArray: true });
+    if (result === undefined) {
+      console.error(result);
+      process.exit(1);
+    }
+    stdout.write(JSON.stringify(result));
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1);
+  }
+});
