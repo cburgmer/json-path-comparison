6eab21b8620a03c3cc6865c70b00e36d341e73b7
Glyn Normington <glyn.normington@gmail.com>
2024-02-23 12:59:54 +0000

    Delete the reference implementation
    It's stalled/incomplete and now archived, so let's drop it to
    avoid confusion. It can always come back later if someone resurrects it.

--- proposals/JSON_Path_Reference_Implementation/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-readonly runner="${script_dir}/build/JSON_Path_Reference_Implementation"
-
-"$runner" "$@"
