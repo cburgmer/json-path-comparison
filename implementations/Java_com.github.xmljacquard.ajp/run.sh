e0d533ba47f011635f80b942585ed53b25a15ac4
Christoph Burgmer <christoph.burgmer@gmail.com>
2025-12-12 10:20:08 +0100

    Fix absolute paths in output for new Java_com.github.xmljacquard.ajp

--- implementations/Java_com.github.xmljacquard.ajp/run.sh
+++ implementations/Java_com.github.xmljacquard.ajp/run.sh
@@ -3,4 +3,8 @@ set -euo pipefail
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -cp "${script_dir}/build/json-path-comparison.jar:$(cat "${script_dir}/build/cp.txt")" query.App "$@"
+filter_absolute_path_in_warning() {
+    sed 's/jar:file:[^ ]*implementations\/Java_com.github.xmljacquard.ajp\//jar:file:.\/implementations\/Java_com.github.xmljacquard.ajp\//'
+}
+
+java -cp "${script_dir}/build/json-path-comparison.jar:$(cat "${script_dir}/build/cp.txt")" query.App "$@" 2>&1 | filter_absolute_path_in_warning
